fixed scaling of bins

src/datasets.py
@@ -12,7 +12,7 @@ class AbSASADataset(Dataset):
 		self.out_file = out_file
 		self.files = data.files
 		# Calculated the minimum and maximum in the train dataset
-		bins = np.linspace(0, 200, num_bins-1)
+		bins = np.linspace(0, 1, num_bins-1)
 		self.num_bins = num_bins
 		self.bins = np.append(bins, np.inf)
 

src/train.py
@@ -98,7 +98,7 @@ def main():
 	# padded values are -1 in labels so do not calculate loss 
 	criterion = nn.CrossEntropyLoss(ignore_index=-1)
 
-	dataset = AbSASADataset("data/relative_area_data/training_data.npz", num_bins=args.num_bins)
+	dataset = AbSASADataset("data/relative_area_data/training_data_normalize.npz", num_bins=args.num_bins)
 	train_split = int(len(dataset) * args.train_val_split)
 	train_dataset, validation_dataset = random_split(dataset, [train_split, len(dataset)-train_split])
 